0b81453da95b7f4ce127feb341aa43115b43eb24
Daggy1234 <arnav.jindal7@gmail.com>
2021-05-26 01:11:16 +0530

    formula fix

--- Formula/dagpi.rb
+++ Formula/dagpi.rb
@@ -1,7 +1,7 @@
 class Dagpi < Formula
   desc "A cli for Managing https://dagpi.xyz"
   homepage "https://github.com/daggy1234/dagpi-cli"
-  url "https://github.com/daggy1234/dagpi-cli/archive/v0.2.0.tar.gz"
+  url "https://github.com/daggy1234/dagpi-cli/archive/v0.2.02.tar.gz"
   sha256 "e4aea37ce06efaf2dc410086d286b3750fe03ef837477701d4490fa09b7a9b24"
   version "0.2.0"
   license "MIT"
